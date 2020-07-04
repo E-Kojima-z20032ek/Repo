1d6280f645b11a1be1a676a09c41c0bb38f86d68
E-Kojima-z20032ek <67811605+E-Kojima-z20032ek@users.noreply.github.com>
2020-07-04 10:53:51 +0900

    第２回システムプログラミング特論で実施

---
+++ fizz_buzz.py
@@ -0,0 +1,15 @@
+
+
+def fb(n):
+    elif n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    if n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+
+
+    i = 1
+    while i <= 20:
+        print(i, fb(i))
+        i = i + 1
